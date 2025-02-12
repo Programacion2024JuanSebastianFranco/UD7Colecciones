18a9836c8551c6ab4e5fcdad182544607ed610c5
juafracas <franxeszian@gmail.com>
2025-02-12 16:49:46 +0100

    INICIO

---
+++ src/ArrayList/Contacto.java
@@ -0,0 +1,75 @@
+package ArrayList;
+
+import java.lang.Comparable;
+import java.util.Objects;
+
+public class Contacto implements Comparable<Contacto> {
+    private String nombre;
+    private String apellido;
+    private String numero;
+
+    private static final String FORMATO_CONTACTO = "%-15s %-15s %-15s";
+    private static final String CABECERA_CONTACTO = String.format(FORMATO_CONTACTO, "Nombre", "Apellidos", "Numero");
+
+    public Contacto(String nombre, String apellidos, String numero) {
+        setNombre(nombre);
+        setApellido(apellidos);
+        setNumero(numero);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede quedar vacio");
+        }
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        if (apellido == null || apellido.trim().isEmpty()) {
+            throw new IllegalArgumentException("los apellidos no puede quedar vacio");
+        }
+        this.apellido = apellido;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        if (numero == null || numero.length() != 9) {
+            throw new IllegalArgumentException("Tiene que ser 9 digitos");
+        }
+
+        try {
+            Integer.valueOf(numero);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("El numero solo puede contener digitos");
+        }
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(FORMATO_CONTACTO, nombre, apellido, numero);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacto contacto = (Contacto) o;
+        return Objects.equals(nombre, contacto.nombre) && Objects.equals(apellido, contacto.apellido) && Objects.equals(numero, contacto.numero);
+    }
+
+    @Override
+    public int compareTo(Contacto o) {
+        return 0;
+    }
+}
