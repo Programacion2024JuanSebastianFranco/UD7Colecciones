0d59e5281b3978e52901a8024b78560e88f1a918
juafracas <franxeszian@gmail.com>
2025-02-21 18:59:00 +0100

    buscarContacto

---
+++ src/MapSet/PalabrasDiferentes/palabrasDiferentes.java
@@ -0,0 +1,17 @@
+package MapSet.PalabrasDiferentes;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class palabrasDiferentes {
+
+    Set<String> palabras = new LinkedHashSet<>();
+
+
+    public void leerFrase(String firts, String  second){
+
+    }
+
+
+
+}
