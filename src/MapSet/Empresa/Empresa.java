ad5266da73156f0b5c1736c4d9552733325a58eb
juafracas <franxeszian@gmail.com>
2025-02-21 18:48:13 +0100

    buscarContacto

--- src/MapSet/Empresa/Empresa.java
+++ src/MapSet/Empresa/Empresa.java
@@ -1,5 +1,6 @@
 package MapSet.Empresa;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,10 +8,47 @@ import java.util.Map;
 public class Empresa {
     Map<String, List<String>> empresa = new HashMap<>();
 
-    public void nuevoEmpleado(String departamento, String empleado){
-        if (!empleado.contains(departamento)){
-            
+    public void nuevoEmpleado(String departamento, String empleado) {
+        if (!empresa.containsKey(departamento)) {
+            empresa.put(departamento, new ArrayList<>());
         }
+
+        List<String> empleados = empresa.get(departamento);
+
+        if (!empleados.contains(empleado)) {
+            empleados.add(empleado);
+        } else {
+            throw new IllegalArgumentException("YA existe el empleado");
+        }
+
+    }
+
+    public void listaDepartamentos() {
+        for (String key : empresa.keySet()) {
+            System.out.println(key);
+        }
+    }
+
+    public void listaEmpleado(String departamento) {
+        if (empresa.containsKey(departamento)) {
+            System.out.println(departamento + ": " + empresa.get(departamento));
+        }
+    }
+
+    public void listaTodos() {
+        for (String key : empresa.keySet()) {
+            System.out.println(key + ": " + empresa.get(key));
+        }
+    }
+
+    public static void main(String[] args) {
+        Empresa empresa = new Empresa();
+        empresa.nuevoEmpleado("Lol", "Salva");
+        empresa.nuevoEmpleado("Lol", "Carlos");
+        empresa.nuevoEmpleado("IT", "Ana");
+        empresa.listaDepartamentos();
+        empresa.listaEmpleado("Lol");
+        empresa.listaTodos();
     }
 
 }
