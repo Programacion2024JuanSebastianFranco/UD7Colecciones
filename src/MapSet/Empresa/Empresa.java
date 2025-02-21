6ddff8fcebafc1fa418f5aa3a9c6dbac4dec82ca
juafracas <franxeszian@gmail.com>
2025-02-21 17:55:08 +0100

    buscarContacto

---
+++ src/MapSet/Empresa/Empresa.java
@@ -0,0 +1,16 @@
+package MapSet.Empresa;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Empresa {
+    Map<String, List<String>> empresa = new HashMap<>();
+
+    public void nuevoEmpleado(String departamento, String empleado){
+        if (!empleado.contains(departamento)){
+            
+        }
+    }
+
+}
--- src/MapSet/Traductor/Traductor.java
+++ src/MapSet/Traductor/Traductor.java
@@ -1,36 +1,35 @@
 package MapSet.Traductor;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.TreeMap;
 
 public class Traductor {
-    Map<String, String> diccionario = new HashMap<>();
-
-    public Traductor(String frase) {
-        this.diccionario = new HashMap<>();
-        String[] palabras = frase.split(" ");
-
-        for (String palabra : palabras){
-            diccionario.put(palabra, diccionario.getOrDefault(palabra, ""));
-        }
-    }
+    TreeMap<String, String> diccionario = new TreeMap<>();
 
     public void nueva(String es, String en) {
-        if (diccionario.containsKey(es)){
+        if (diccionario.containsKey(es) || diccionario.containsKey(en)){
             diccionario.replace(es, en);
-        } else if (diccionario.containsKey(en)){
-            diccionario.replace(en,es);
         } else {
             diccionario.put(es,en);
         }
     }
 
-    public void eliminar(String es, String en) {
+    public void eliminar(String es) {
         if (diccionario.containsKey(es)){
-            diccionario.remove(es, en);
-        } else if (diccionario.containsKey(en)){
-            diccionario.remove(en,es);
-        } 
+            diccionario.remove(es);
+        } else {
+            throw new IllegalArgumentException("No existe");
+        }
+    }
+
+    public String traducir(String frase){
+        String[] palabras = frase.split(" ");
+        StringBuilder salida = new StringBuilder();
+
+        for (String palabra : palabras) {
+            salida.append(diccionario.get(palabra)).append(" ");
+        }
+
+        return salida.toString();
     }
 
     public void mostrarLista(){
@@ -40,8 +39,15 @@ public class Traductor {
     }
 
     public static void main(String[] args) {
-        Traductor traductor = new Traductor("Hola Hello");
+        Traductor traductor = new Traductor();
+        traductor.nueva("Perro", "Dog");
+        traductor.nueva("Hola", "Hello");
+        traductor.nueva("Buen", "Good");
+        traductor.nueva("Feliz", "Happy");
         traductor.mostrarLista();
-        
+        traductor.eliminar("Feliz");
+        traductor.mostrarLista();
+        String trad = traductor.traducir("Hola Buen Perro");
+        System.out.println(trad);
     }
 }
